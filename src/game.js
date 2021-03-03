13c7de85e6372c3ecce88721e54f52a1575eb668
schlanjo <schlanjo@gmail.com>
2021-03-03 15:40:45 -0700

    Establish win condition methods and build out row checking

--- src/game.js
+++ src/game.js
@@ -1,15 +1,59 @@
+var Player = require('./player.js');
+
 class Game {
     constructor() {
         this.playerOne = new Player("one", "heart");
         this.playerTwo = new Player("two", "raven");
+        // this.boardGrid = [
+        //     { cell1: "" }, { cell2: "" }, { cell3: "" },
+        //     { cell4: "" }, { cell5: "" }, { cell6: "" },
+        //     { cell7: "" }, { cell8: "" }, { cell9: "" } 
+        // ]
+        this.boardGrid = [
+            null, null, null,
+            null, null, null,
+            null, null, null
+        ]
         this.currentTurn = this.playerOne.id;
     }
 
+    placeToken(cell, token) {
+        this.boardGrid.splice(cell, 1, token);
+    }
+    
     verifyWin() {
 
     }
 
     verifyDraw() {
+
+    }
+
+    checkRows() {
+        var currentGrid = this.boardGrid;
+        var row;
+
+        for (var i = 0; i < 3; i++) {
+            row = currentGrid.splice(0, 3);
+            console.log(row);
+            
+            if ((row[0] === null) && ((row[0] === row[1]) && (row[0] === row[2]))) {
+                continue;
+            }
+
+            if ((row[0] === row[1]) && (row[0] === row[2])) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    checkColumns() {
+
+    }
+
+    checkDiagonals() {
 
     }
 
@@ -21,3 +65,10 @@ class Game {
 
     }
 }
+
+var game = new Game();
+game.placeToken(2, game.playerOne.token);
+game.placeToken(5, game.playerOne.token);
+game.placeToken(8, game.playerOne.token);
+
+console.log(game.checkRows());
--- src/main.js
+++ src/main.js
@@ -2,5 +2,23 @@
 
 // Event Listeners
 
+
+
+
+
+
 // Event Handlers/Helper Functions
 
+/* Build Win State Functionality */
+
+// Desc: Players take turns placing their respective
+// icons on the 9 x 9 board until one of has a consecutive
+// row, column, or diagonal of 3 icons. The player that
+// does this is the winner.
+
+// Input: Player positions on a 9x9 grid.
+// Output: Player who has 3 consecutive icons place in 
+// a row, column, or diagonal.
+
+// Steps: 
+// - declare function that takes in 
