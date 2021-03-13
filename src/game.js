d2d18c79fad812925100b13e89c7f5309b44d327
schlanjo <schlanjo@gmail.com>
2021-03-13 09:19:00 -0700

    Establish win states property

--- src/game.js
+++ src/game.js
@@ -9,7 +9,12 @@ class Game {
       null, null, null, 
       null, null, null,
       null, null, null
-    ]
+    ];
+    this.winStates = [
+      [0, 1, 2], [3, 4, 5], [6, 7, 8],
+      [0, 3, 6], [1, 4, 7], [2, 5, 8],
+      [0, 4, 8], [2, 4, 6]
+    ];
   }
 
   placeToken(squareIndex, token) {
